20aff2cfc425bb5e14596dfa2786506f3fa990e4
kgi04 <kimguwonil1020@gmail.com>
2025-03-11 19:53:04 +0900

    fuck

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,70 +1,25 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-// fuck
-public class Ghost : MonoBehaviour
+public class Ghost : Block
 {
-    public Board Board;
     public Tile GhostTile;
-    public Block TrackingBlock;
-    public Tilemap GhostTilemap;
 
-    public Vector3Int[] cells { get; private set; }
-    public Vector3Int pos { get; private set; }
-
-    private void Awake()
+    public override void Initialize(TetrominoData _data, Vector3Int _pos)
     {
-        cells = new Vector3Int[4];
-    }
+        data = _data;
+        pos = _pos;
 
-    private void LateUpdate()
-    {
-        DeleteBlock();
-        Copy();
-        Drop();
-        Render();
-    }
+        _data.tile = GhostTile;
 
-    private void DeleteBlock()
-    {
-        GhostTilemap.ClearAllTiles();
-    }
-
-    private void Copy()
-    {
-        for (int i = 0; i < TrackingBlock.cells.Length; ++i)
+        if (cells == null)
         {
-            cells[i] = TrackingBlock.cells[i];
+            cells = new Vector3Int[_data.cells.Length];
         }
-    }
 
-    private void Drop()
-    {
-        Vector3Int position = TrackingBlock.pos;
-
-        int current_y = position.y;
-        int groundYpos = -10;
-
-        for (int i = current_y; i >= groundYpos; --i)
+        for (int i = 0; i < _data.cells.Length; ++i)
         {
-            position.y = i;
-
-            if (Board.CanMove(TrackingBlock, Vector3Int.down))
-            {
-                pos = position;
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
-
-    private void Render()
-    {
-        for (int i = 0; i < cells.Length; ++i)
-        {
-            GhostTilemap.SetTile(pos + cells[i], GhostTile);
+            cells[i] = (Vector3Int)_data.cells[i];
         }
     }
 }
